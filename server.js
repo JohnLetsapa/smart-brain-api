96fdc12b9fba16cb11b7d02b0aea28e3dba559c0
Letsapa Letsapa <letsapaletsapa@Letsapas-MacBook-Pro.local>
2022-02-23 16:15:37 +0200

    endpoints

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'https://salty-scrubland-21115.herokuapp.com/',
+      host : 'localhost',
       user : '',
       password : '',
       database : 'smart-brain-db'
@@ -23,11 +23,11 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/', (req, res) => res.send('It is running!'))
-app.post('https://salty-scrubland-21115.herokuapp.com/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt))  
-app.post('https://salty-scrubland-21115.herokuapp.com/register', (req,res) => register.handleRegister(req, res, db, bcrypt))
-app.get('https://salty-scrubland-21115.herokuapp.com/profile/:id', (req, res) => profile.handleProfile(req, res, db)) 
-app.put('https://salty-scrubland-21115.herokuapp.com/image', (req, res) => image.handleImage(req, res, db))
-app.post('https://salty-scrubland-21115.herokuapp.com/imageurl', (req, res) => image.handleApiCall(req, res))
+app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt))  
+app.post('/register', (req,res) => register.handleRegister(req, res, db, bcrypt))
+app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db)) 
+app.put('/image', (req, res) => image.handleImage(req, res, db))
+app.post('/imageurl', (req, res) => image.handleApiCall(req, res))
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`)
